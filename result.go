9f376e510632c94f38c3ee5551128755bb18e640
lemon-mint <ice.lemons.mint@gmail.com>
2022-03-19 09:03:44 +0900

    Result

---
+++ result.go
@@ -0,0 +1,41 @@
+package m
+
+type Result[T any] struct {
+	v   T
+	err error
+}
+
+func Ok[T any](v T) Result[T] {
+	return Result[T]{v: v}
+}
+
+func Err[T any](err error) Result[T] {
+	return Result[T]{err: err}
+}
+
+func (r Result[T]) IsOk() bool {
+	return r.err == nil
+}
+
+func (r Result[T]) IsErr() bool {
+	return r.err != nil
+}
+
+func (r Result[T]) Value() (T, error) {
+	return r.v, r.err
+}
+
+func (r Result[T]) Unwrap() T {
+	if r.IsErr() {
+		panic("Unwrap called on Err")
+	}
+	return r.v
+}
+
+func (r Result[T]) Match(onOk func(v T), onErr func(err error)) {
+	if r.IsOk() {
+		onOk(r.v)
+	} else {
+		onErr(r.err)
+	}
+}
